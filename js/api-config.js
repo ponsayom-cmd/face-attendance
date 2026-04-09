53df7ff04e7f168b5d78bb5890ad393b37b57f55
ponsayom-cmd <ponsayom@gmail.com>
2026-04-09 16:21:50 +0700

    Add files via upload

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxHPUPzoCgYPaekGkGpsbTc5cf-vqt2Je1rqdWdK1W8Dw-iQcrikxy3ix3yioul2UppOA/exec';
