768ef3c03b5957678b16f207a2cfce326e66f98f
ponsayom-cmd <ponsayom@gmail.com>
2026-04-10 21:58:02 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby9RgaD6r-xUoPkb_8uIUaY4r2XfC0kOh5AUq8d8kun8jBHRXK2WiAkht2UuSOV10B3jw/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzN-QfvrWmAqG5D2RhqS_mo2n0ufLEzFmR9tAhmC74wJwhL7yNRJt9XoAtwygyZI8d_/exec';
